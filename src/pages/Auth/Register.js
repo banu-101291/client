cb2d8b09970dc0c287dbe534a2fb9d4280e22723
banu-101291 <sifnaz78@gmail.com>
2024-02-12 12:49:16 +0530

    category

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import "../../styles/AuthStyles.css";
+
+
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -17,7 +19,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/register", {
+      const res = await axios.post("https://ecomss.onrender.com/api/v1/auth/register", {
         name,
         email,
         password,
--- src/pages/Policy.js
+++ src/pages/Policy.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout";
-
+import img from '../images/contactus.jpeg'
 const Policy = () => {
   return (
     <Layout title={"Privacy Policy"}>
       <div className="row contactus ">
         <div className="col-md-6 ">
           <img
-            src="/images/contactus.jpeg"
+            src={img}
             alt="contactus"
-            style={{ width: "100%" }}
+            style={{ width: "85%" }}
           />
         </div>
         <div className="col-md-4">
